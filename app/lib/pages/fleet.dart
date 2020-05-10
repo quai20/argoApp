1d574c87ba15a7ff5aca186ff02fd89eb2a79cf2
quai20 <kevin.balem@gmail.com>
2020-05-10 14:57:56 +0200

    add fleet saving

--- app/lib/pages/fleet.dart
+++ app/lib/pages/fleet.dart
@@ -1,12 +1,68 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class Fleet extends StatelessWidget {
+class Fleet extends StatefulWidget {
+  @override
+  _FleetState createState() => new _FleetState();
+}
+
+class _FleetState extends State<StatefulWidget> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: Text("My fleet"),
         ),
-        body: new Container());
+        body: FutureBuilder<List<String>>(
+            // get the wmofleetlist, saved in the preferences
+            future: SharedPreferencesHelper.getwmofleet(),
+            initialData: List<String>(),
+            builder:
+                (BuildContext context, AsyncSnapshot<List<String>> snapshot) {
+              return snapshot.hasData ? _buildList(snapshot.data) : Container();
+            }));
+  }
+
+  Widget _buildList(List<String> wmolist) {    
+
+    return new ListView.builder(
+        itemCount: wmolist.length,
+        itemBuilder: (BuildContext ctxt, int index) {          
+          return new ListTile(
+              title: Text(wmolist[index],
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+              trailing: IconButton(
+                  icon: Icon(Icons.remove_circle_outline, color: Colors.black),
+                  onPressed: () async {
+                    wmolist.remove(wmolist[index]);
+                    await SharedPreferencesHelper.setwmofleet(wmolist);
+                    setState(() {});
+                  }));
+        });
+  }
+}
+
+class SharedPreferencesHelper {
+  ///
+  /// Instantiation of the SharedPreferences library
+  ///
+  static final String _kwmofleet = "wmofleet";
+
+  /// ------------------------------------------------------------
+  /// Method that returns the saved wmos, or empty list if none
+  /// ------------------------------------------------------------
+  static Future<List<String>> getwmofleet() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    return prefs.getStringList(_kwmofleet) ?? List<String>();
+  }
+
+  /// ----------------------------------------------------------
+  /// Method that saves the fleet
+  /// ----------------------------------------------------------
+  static Future<bool> setwmofleet(List<String> value) async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    return prefs.setStringList(_kwmofleet, value);
   }
 }
--- app/lib/pages/wmo.dart
+++ app/lib/pages/wmo.dart
@@ -1,15 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class Wmo extends StatelessWidget {
+class Wmo extends StatefulWidget {
+  @override
+  _WmoState createState() => new _WmoState();
+}
+
+class _WmoState extends State<StatefulWidget> {
   @override
   Widget build(BuildContext context) {
+    //Get wmo clicked
     List wmodata = ModalRoute.of(context).settings.arguments;
 
     return Scaffold(
         appBar: AppBar(
-          title: Text("Float : " + wmodata[0].toString()),
-        ),
+            title: Text("Float : " + wmodata[0].toString()),
+            actions: <Widget>[
+              FutureBuilder<List<String>>(
+                  // get the wmofleetlist, saved in the preferences
+                  future: SharedPreferencesHelper.getwmofleet(),
+                  initialData: List<String>(),
+                  builder: (BuildContext context,
+                      AsyncSnapshot<List<String>> snapshot) {
+                    return snapshot.hasData
+                        ? _buildIcon(snapshot.data, wmodata[0].toString())
+                        : Container();
+                  }),
+            ]),
         body: Center(
             child: ListView(
           padding: const EdgeInsets.all(8),
@@ -43,14 +61,61 @@ class Wmo extends StatelessWidget {
             Container(
               height: 400,
               child: CachedNetworkImage(
-                imageUrl: 'http://www.ifremer.fr/erddap/tabledap/ArgoFloats.png?longitude,latitude,time&platform_number=%22' +
-                      wmodata[0].toString() +
-                      '%22&.draw=linesAndMarkers&.marker=5%7C5&.color=0x000000&.colorBar=KT_thermal%7C%7C%7C%7C%7C&.bgColor=0xffccccff',
+                imageUrl:
+                    'http://www.ifremer.fr/erddap/tabledap/ArgoFloats.png?longitude,latitude,time&platform_number=%22' +
+                        wmodata[0].toString() +
+                        '%22&.draw=linesAndMarkers&.marker=5%7C5&.color=0x000000&.colorBar=KT_thermal%7C%7C%7C%7C%7C&.bgColor=0xffccccff',
                 placeholder: (context, url) => new CircularProgressIndicator(),
                 errorWidget: (context, url, error) => new Icon(Icons.error),
               ),
             )
           ],
         )));
+  }
+
+  Widget _buildIcon(List<String> wmolist, String wmo) {
+    if (wmolist.contains(wmo)) {
+      //return Icon(Icons.favorite, color: Colors.red);
+      return IconButton(
+          icon: Icon(Icons.favorite, color: Colors.red),
+          onPressed: () async {
+            wmolist.remove(wmo);
+            await SharedPreferencesHelper.setwmofleet(wmolist);
+            setState(() {});
+          });
+    } else {
+      return IconButton(
+          icon: Icon(Icons.favorite, color: Colors.white),
+          onPressed: () async {
+            wmolist.add(wmo);
+            await SharedPreferencesHelper.setwmofleet(wmolist);
+            setState(() {});
+          });
+    }    
+  }
+}
+
+class SharedPreferencesHelper {
+  ///
+  /// Instantiation of the SharedPreferences library
+  ///
+  static final String _kwmofleet = "wmofleet";
+
+  /// ------------------------------------------------------------
+  /// Method that returns the saved wmos, or empty list if none
+  /// ------------------------------------------------------------
+  static Future<List<String>> getwmofleet() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    return prefs.getStringList(_kwmofleet) ?? List<String>();
+  }
+
+  /// ----------------------------------------------------------
+  /// Method that saves the fleet
+  /// ----------------------------------------------------------
+  static Future<bool> setwmofleet(List<String> value) async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    return prefs.setStringList(_kwmofleet, value);
   }
 }
